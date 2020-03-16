22bf21a30a8c0edb453e420d4d82c23c98e88b80
Prashant Bhardwaj <amu.prashant@gmail.com>
2020-03-16 02:10:57 +0000

    first commit

---
+++ src/main/java/com/prashant/app/CamelSpringJmsKafkaApplication.java
@@ -0,0 +1,33 @@
+package com.prashant.app;
+
+import org.apache.qpid.jms.JmsConnectionFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.jms.connection.CachingConnectionFactory;
+
+@SpringBootApplication
+public class CamelSpringJmsKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamelSpringJmsKafkaApplication.class, args);
+	}
+
+	@Bean
+	public JmsConnectionFactory jmsConnectionFactory(@Value("qpidUser") String qpidUser, @Value("qpidPassword") String qpidPassword, @Value("qpidBrokerUrl") String qpidBrokerUrl) {
+		JmsConnectionFactory jmsConnectionFactory = new JmsConnectionFactory(qpidPassword, qpidPassword, qpidBrokerUrl);
+		return jmsConnectionFactory;
+	}
+
+	@Bean
+	@Primary
+	public CachingConnectionFactory jmsCachingConnectionFactory(JmsConnectionFactory jmsConnectionFactory) {
+		CachingConnectionFactory cachingConnectionFactory = new CachingConnectionFactory(jmsConnectionFactory);
+
+		return cachingConnectionFactory;
+	}
+
+
+}
---
+++ src/main/java/com/prashant/app/QpidToKafkaRoute.java
@@ -0,0 +1,13 @@
+package com.prashant.app;
+
+import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class QpidToKafkaRoute extends RouteBuilder {
+
+    public void configure() throws Exception {
+        from("amqp:queue:toKafka")
+                .to("kafka:camel");
+    }
+}
