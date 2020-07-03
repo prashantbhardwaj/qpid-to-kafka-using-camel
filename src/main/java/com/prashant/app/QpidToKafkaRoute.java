4903c99764b71c09a014273f092b51bddc4797e3
Prashant Bhardwaj <amu.prashant@gmail.com>
2020-07-03 02:59:13 +0100

    added log and header in kafka message

--- src/main/java/com/prashant/app/QpidToKafkaRoute.java
+++ src/main/java/com/prashant/app/QpidToKafkaRoute.java
@@ -1,6 +1,7 @@
 package com.prashant.app;
 
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.kafka.KafkaConstants;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -8,6 +9,9 @@ public class QpidToKafkaRoute extends RouteBuilder {
 
     public void configure() throws Exception {
         from("amqp:queue:test")
-                .to("kafka:camel");
+                .log("Received key : ${header.JMSMessageID}, message : ${body}")
+                .setHeader(KafkaConstants.KEY, header("JMSMessageID"))
+                .to("kafka:camel")
+                .log("Sent key : ${headers[kafka.KEY]}, message : ${body}");
     }
 }
